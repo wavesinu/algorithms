bdff29cc5b958f47b10ff38f1c74d85be7a31a7c
ISU <wavesinu@gmail.com>
2024-02-24 19:12:35 +0900

    CodeTree test examples

--- Code-Tree/Dynamic Programming/계단 오르기.py
+++ Code-Tree/Dynamic Programming/계단 오르기.py
@@ -1,20 +1,15 @@
-def count_ways(n, memo=None):
-    if memo is None:
-        memo = [-1] * (n + 1)
-
-    if n == 0 or n == 1:
-        return 0
-    if n == 2:
-        return 1
-    if memo[n] != -1:
-        return memo[n]
-    memo[n] = count_ways(n-2, memo) + count_ways(n-3, memo)
-    return memo[n] % 10007
+MAX_N = 1000
+MOD = 10007
 
 n = int(input())
+dp = [0] * (MAX_N + 1)
 
-result = count_ways(n)
-if result > 0:
-    print(result)
-else:
-    print(0)
+dp[0] = 1
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, n + 1):
+    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD
+
+print(dp[n])
