601d6608f5ad97df4c3538fc6734d8fac425afe6
ISU <wavesinu@gmail.com>
2024-02-28 19:22:49 +0900

    Update 문자열 내 p와 y의 개수.py

--- Programmers/level 1/문자열 내 p와 y의 개수.py
+++ Programmers/level 1/문자열 내 p와 y의 개수.py
@@ -1,1 +1,19 @@
 def solution(s):
+    new_string = s.upper()
+    count_p, count_y = 0, 0
+
+    for i in new_string:
+        if i == 'P':
+            count_p += 1
+        elif i == 'Y':
+            count_y += 1
+
+    if count_p != count_y:
+        return "false"
+    else:
+        return "true"
+    return True
+
+
+def solution_v2(s):
+    return s.upper().count('P') == s.upper().count('Y')
