7ae055989036a7120d50ae3a9c0f6c601b28fe05
ISU <wavesinu@gmail.com>
2024-02-28 19:23:34 +0900

    Create 신규 아이디 추천.py
    입력값이 "123_.def"일 때 "_.def" 출력되는 문제가 발생

---
+++ Programmers/level 1/신규 아이디 추천.py
@@ -0,0 +1,39 @@
+import re
+
+
+def solution(user_id: str) -> str:
+    new_id = user_id.lower()
+    symbols = set(["-", "_", "."])
+
+    def is_valid_char(char, symbols):
+        return char.isalpha() or char in symbols
+
+    # symbols와 다른 기호를 삭제
+    new_id = ''.join(char for char in new_id if is_valid_char(char, symbols))
+
+    # 마침표가 2번 이상 연속된 부분을 하나의 마침표로 치환
+    new_id = re.sub('\.+', '.', new_id)
+
+    # 마침표가 처음이나 끝에 위치한다면 제거
+    new_id = new_id.strip('.')
+
+    # new_id가 빈문자열이라면 new_id에 a를 대입
+    if new_id == '':
+        new_id = 'a'
+
+    # new_id의 길이가 16자 이상이라면, new_id의 첫 15개 문자를 제외한 나머지 문자들을 모두 제거
+    # 만약 제거 후, 마침표가 new_id의 끝에 위치한다면 끝에 위치한 마침표 제거
+
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+        new_id = new_id.rstrip('.')
+
+    # new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙임
+    while len(new_id) < 3 and new_id:
+        new_id += new_id[-1]
+
+    return new_id
+
+
+a = input()
+print(solution(a))
