b14de5cd6ff6ee4d375e7c97897ecbe56c061267
ISU <wavesinu@gmail.com>
2024-09-20 10:26:00 +0900

    Update dir path

--- leet-code/Top 100 Liked/37-Search Insert Position.py
+++ leet-code/Top 100 Liked/37-Search Insert Position.py
@@ -1,17 +1,18 @@
 from typing import List
 
+
 class Solution:
-    def binary_search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) -1
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
 
         while left <= right:
-            mid = left + (right - left) // 2
+            mid = (left + right) // 2
 
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 left = mid + 1
-            elif nums[mid] > target:
+            else:
                 right = mid - 1
 
-        return -1
+        return left
